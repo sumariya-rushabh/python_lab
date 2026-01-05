a595275302943d1628f08109696e1e29ab6ea235
sumariya-rushabh <sumariyarushabh09@gmail.com>
2026-01-05 11:10:16 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 15
+print(x)
+15
+x = 10
+print(x)
+10
+x = "hello"
+print(x)
+hello
+hello
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    hello
+NameError: name 'hello' is not defined. Did you mean: 'help'?
+a = ["apple","mengo","cherry"]
+print(a)
+['apple', 'mengo', 'cherry']
+['apple', 'mengo', 'cherry']
+['apple', 'mengo', 'cherry']
+
+b = ("apple","mengo","cherry")
+print(b)
+('apple', 'mengo', 'cherry')
+y = 1j
+print(y)
+1j

4d098780c4751d943dd556131fa0403b340ed452
sumariya-rushabh <sumariyarushabh09@gmail.com>
2026-01-05 11:42:45 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,55 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a="hello world"
+print(a)
+hello world
+b=99
+print(b)
+99
+c=99.9
+print(c)
+99.9
+d=1R;
+SyntaxError: invalid decimal literal
+d=ij
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    d=ij
+NameError: name 'ij' is not defined. Did you mean: 'id'?
+d=1j;
+print(d)
+1j
+x=["RUSHABH","CHIRAG","SUMARIYA"]
+print(x)
+['RUSHABH', 'CHIRAG', 'SUMARIYA']
+y=
+KeyboardInterrupt
+y=("RUSHABH","CHIRAG","SUMARIYA")
+print(y)
+('RUSHABH', 'CHIRAG', 'SUMARIYA')
+z=range(7)
+print(z)
+range(0, 7)
+x={"name":"RUSHABH","age":18}
+print(x)
+{'name': 'RUSHABH', 'age': 18}
+a={"mango","apple","banana"}
+print(a)
+{'apple', 'mango', 'banana'}
+b=frozenset({"mango","apple","banana"})
+print(b)
+frozenset({'apple', 'mango', 'banana'})
+x="true"
+print(x)
+true
+x=A"hello"
+SyntaxError: invalid syntax
+x=a "hello"
+SyntaxError: invalid syntax
+x=b"hello"
+print(x)
+b'hello'
+x=bytearray(7)
+print(x)
+bytearray(b'\x00\x00\x00\x00\x00\x00\x00')
+\

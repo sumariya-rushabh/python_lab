9b337d7f20abd7534074376206ea1a5bb3cbad90
sumariya-rushabh <sumariyarushabh09@gmail.com>
2026-01-19 20:56:17 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,80 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Welcome to Python Programming!")
+Welcome to Python Programming!
+print("\n\tRajesh Kumar\n\tFlat No.101, Sunshine Apartments\n\tMG Road, Sector15\n\tRajkot\n\tPincode: 360004\n\tIndia.")
+
+	Rajesh Kumar
+	Flat No.101, Sunshine Apartments
+	MG Road, Sector15
+	Rajkot
+	Pincode: 360004
+	India.
+a = 150
+b = 120.50
+print("Addition:", a + b)
+Addition: 270.5
+print("Subtraction:", a - b)
+Subtraction: 29.5
+print("Multiplication:", a * b)
+Multiplication: 18075.0
+print("Division:", a / b)
+Division: 1.2448132780082988
+radius = float(input("Enter radius of the circle: "))area = 3.14 * radius * radiuscircumference = 2 * 3.14 * radiusprint("Area of Circle:", area)print("Circumference of Circle:", circumference)
+SyntaxError: invalid syntax
+radius = float(input("Enter radius of the circle: "))
+Enter radius of the circle: 10
+area = 3.14 * radius * radius
+circumference = 2 * 3.14 * radius
+print("Area of Circle:", area)
+Area of Circle: 314.0
+print("Circumference of Circle:", circumference)
+Circumference of Circle: 62.800000000000004
+P = float(input("Enter Principal amount: "))
+Enter Principal amount: 123
+R = float(input("Enter Rate of Interest: "))
+Enter Rate of Interest: 123
+T = float(input("Enter Time (in years): "))
+Enter Time (in years): 123
+simple_interest = (P * R * T) / 100
+print("Simple Interest:", simple_interest)
+Simple Interest: 18608.67
+length = float(input("Enter length of rectangle: "))
+Enter length of rectangle: 99
+width = float(input("Enter width of rectangle: "))
+Enter width of rectangle: 9
+perimeter = 2 * (length + width)
+print("Perimeter of Rectangle:", perimeter)
+Perimeter of Rectangle: 216.0
+length = float(input("Enter length: "))
+Enter length: 11
+width = float(input("Enter width: "))
+Enter width: 9
+area = length * width
+perimeter = 2 * (length + width)
+print("Area of Rectangle:", area)
+Area of Rectangle: 99.0
+print("Perimeter of Rectangle:", perimeter)
+Perimeter of Rectangle: 40.0
+a = float(input("Enter side a: "))
+Enter side a: 1234
+b = float(input("Enter side b: "))
+Enter side b: 2
+c = float(input("Enter side c: "))
+Enter side c: 999
+perimeter = a + b + c
+print("Perimeter of Triangle:", perimeter)
+Perimeter of Triangle: 2235.0
+side = float(input("Enter side of square: "))
+Enter side of square: 99
+area = side * side
+perimeter = 4 * side
+print("Area of Square:", area)
+Area of Square: 9801.0
+print("Perimeter of Square:", perimeter)
+Perimeter of Square: 396.0
+side = float(input("Enter side of square: "))
+Enter side of square: 123
+perimeter = 4 * side
+print("Perimeter of Square:", perimeter)
+Perimeter of Square: 492.0

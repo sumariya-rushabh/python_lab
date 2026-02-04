5ef6bd80f5db16305cefe1142de2076d99f4a672
sumariya-rushabh <sumariyarushabh09@gmail.com>
2026-02-04 08:20:29 +0530

    Add files via upload

---
+++ ios.py
@@ -0,0 +1,17 @@
+"""a = 10
+print(a)
+
+
+name=input("ENTER YOUR NAME:")
+print(name)
+
+age=int(input("ENTER YOUR AGE:"))
+print(age)
+
+
+name= input("enter your name:")
+print( "hello" ,name)
+
+
+x= True
+print(x)"""

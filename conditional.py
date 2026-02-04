91b24279e078216fd970bcd363af282e045ee5af
sumariya-rushabh <sumariyarushabh09@gmail.com>
2026-02-04 08:45:46 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,44 @@
+#Q.1 Write a Python program to check if a person is eligible to vote based on their age.
+
+"""age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible.")
+else:
+    print("You are not eligible.")"""
+    
+#Q.2 write a python program to find marks grade based on score.
+
+"""score = int(input("Enter your score: "))
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: D")"""
+    
+#Q.3 Write a Python program to check if a number is positive, negative.
+"""num = float(input("Enter a number: "))
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")"""
+    
+#Q.4 Write a python program on license eligibility based on age and country.
+"""age = int(input("Enter your age: "))
+country = input("Enter your country: ")
+if age >= 18 and country == "INDIA":
+    print("You are eligible for a driver's license.")
+else:
+    print("You are not eligible for a driver's license.")"""
+    
+#Q.5 Write a python program on odd or even number.
+"""num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")"""
+    

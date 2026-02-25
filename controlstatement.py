454b6ee0b3d3143854ef1ed174b1d345f4045743
sumariya-rushabh <sumariyarushabh09@gmail.com>
2026-02-25 08:38:40 +0530

    Add files via upload

---
+++ controlstatement.py
@@ -0,0 +1,34 @@
+#Q.1 Print even numbers from 1 to 20
+"""for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+#Q.2 Print odd numbers from 1 to 15
+for i in range(1, 16):
+    if i % 2 != 0:
+        print(i)
+
+#Q.3 Print table of 5
+for i in range(1, 11):
+    print(f"5 x {i} = {5*i}")
+
+#Q.4 Print no from 1 to 10
+for i in range(1, 11):
+    print(i)
+
+#Q.5 Print characters of a string
+name="RUSHABH"
+for letter in name:
+    print(letter)
+
+
+#Q.6 Sum of number from 1 to 5 total = 0
+total = 0
+for i in range(1, 6):
+    total += i  
+print("Sum is:", total)
+
+#Q.7 Print list elements
+numbers = [10, 20, 30, 40, 50]
+for n in numbers:
+    print(n)"""

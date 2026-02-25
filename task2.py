7341c8214dc3fe725d57e4faa45c9eb1427c8cbe
sumariya-rushabh <sumariyarushabh09@gmail.com>
2026-02-25 08:20:57 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,52 @@
+#Q.1 Print number from 1 to 5
+"""i=1
+while i<=5:
+    print(i)
+    i=i+1
+
+#Q.2 Sum of Number take value from user
+n = int(input("Enter a number: "))
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+print("Sum of first", n, "natural numbers is:", sum)
+
+#Q.3 Print odd numbers from 1 to 20
+i = 1
+while i <= 20:
+    if i % 2 != 0:
+        print(i)
+    i = i + 1
+
+#Q.4 Print reverse number from 10 to 1
+i = 10
+while i >= 1:
+    print(i)
+    i = i - 1
+
+#Q.5 Find largest number in a list
+numbers = [3, 7, 2, 9, 5]
+largest = numbers[0]
+i = 1
+while i < len(numbers):
+    if numbers[i] > largest:
+        largest = numbers[i]
+    i = i + 1
+print("Largest number in the list is:", largest)
+
+#Q.6 Print table for 4 with while loop
+num = 4
+i = 1
+while i <= 10:
+    print(num, "x", i, "=", num * i)
+    i = i + 1
+
+
+#Q.7 Print even numbers from 1 to 20
+i = 1
+while i <= 20:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1"""
